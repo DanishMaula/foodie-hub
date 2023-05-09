820f1e51e9456aa6c220b7b4c92470e5017b2da8
Danish <danishmaulahasbi@gmail.com>
2023-05-09 09:38:16 +0700

    feat: add list for restaurant (unfinished)

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        
+        scaffoldBackgroundColor: Colors.white),
       home: const HomePage(),
       routes: {
         HomePage.routeName: (context) => const HomePage(),
--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,6 +1,5 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:foodie_hub/utils/style_manager.dart';
 
 class HomePage extends StatelessWidget {
   static const String routeName = '/home-page';
@@ -8,6 +7,53 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Welcome to Foodie Hub',
+                    style: getBlackTextStyle(
+                        fontSize: 24, fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    'Your one stop for all your food needs.',
+                    style: getBlackTextStyle(),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24),
+              Expanded(
+                child: _buildList(context),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  FutureBuilder<String> _buildList(BuildContext context) {
+    return FutureBuilder(
+        future: DefaultAssetBundle.of(context)
+            .loadString('assets/local_restaurant.json'),
+        builder: (context, snapshot) {
+          return ListView.builder(
+              shrinkWrap: true,
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.only(bottom: 12),
+                  height: 100,
+                  color: Colors.red,
+                );
+              });
+        });
   }
 }
