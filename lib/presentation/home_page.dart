1e91d2cba4348e9d8159b18a5202b75c2129bab9
Danish <danishmaulahasbi@gmail.com>
2023-05-09 10:42:52 +0700

    feat: finish listview

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -58,65 +58,82 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _buildRestaurantItem(BuildContext context, Restaurant restaurant) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: Container(
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: const Offset(0, 3), // changes position of shadow
-                ),
-              ],
-            ),
-            child: Hero(
-              tag: restaurant.pictureId,
-              child: Image.network(
-                restaurant.pictureId,
-                height: 150,
-                width: 145,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          flex: 2,
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  restaurant.name,
-                  style: getBlackTextStyle(),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  restaurant.description,
-                  style: getBlackTextStyle(fontSize: 12),
-                  maxLines: 4,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Row(
-                  children: [
-                    const Icon(Icons.star, color: Colors.yellow),
-                    Text(
-                      restaurant.rating.toString(),
-                      style: getBlackTextStyle(fontSize: 12),
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () {},
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset:
+                            const Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Hero(
+                    tag: restaurant.pictureId,
+                    child: Image.network(
+                      restaurant.pictureId,
+                      height: 150,
+                      width: 145,
+                      fit: BoxFit.cover,
                     ),
-                  ],
-                )
-              ],
-            ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        restaurant.name,
+                        style: getBlackTextStyle(fontWeight: FontWeight.w600),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        restaurant.description,
+                        style: getBlackTextStyle(
+                            fontSize: 13, fontWeight: FontWeight.w500),
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: <Widget>[
+                          const Icon(
+                            Icons.location_on,
+                            color: Colors.grey,
+                            size: 17,
+                          ),
+                          const SizedBox(width: 4),
+                          Text(
+                            restaurant.city,
+                            style: getBlackTextStyle(
+                                fontSize: 12, fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
+        const SizedBox(height: 14),
       ],
     );
   }
